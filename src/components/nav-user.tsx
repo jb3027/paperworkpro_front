2ba8b50f379ca1ded871f7938d7d671cb059d435
jb3027 <146097465+jb3027@users.noreply.github.com>
2025-10-07 16:15:15 +0100

    UI folder fixed

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -68,6 +68,7 @@ export function NavUser({
             align="end"
             sideOffset={4}
           >
+            
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
